1eda284b77511f1eb221039c0f74810af8c6fcc1
Unbound <f1arenova1@gmail.com>
2021-07-30 14:36:46 +0300

    refactor index into express app comp

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
+const app = require("./app");
+const http = require("http");
 const config = require("./utils/config");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
-app.use(cors());
-app.use(express.json());
-app.use("/api/blogs", blogsRouter);
 
-app.listen(config.PORT, () => {
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
