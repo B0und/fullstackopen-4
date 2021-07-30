dc154c6ac5961db9312c3beb49c67090b936b394
Unbound <f1arenova1@gmail.com>
2021-07-30 18:04:08 +0300

    added favourite blog helper and tests 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,6 +9,17 @@ module.exports = {
 const totalLikes = (blogs) =>
   blogs.reduce((likesSum, blog) => likesSum + blog.likes, 0);
 
+const favouriteBlog = (blogs) =>
+  blogs
+    .map((blog) => ({
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes,
+    }))
+    .reduce((max, blog) => (max.likes > blog.likes ? max : blog));
+
 module.exports = {
-  dummy, totalLikes
+  dummy,
+  totalLikes,
+  favouriteBlog,
 };
