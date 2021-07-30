64a6b14b1a1fc21e8ebe1e244964fab0f38bf0d4
Unbound <f1arenova1@gmail.com>
2021-07-30 14:26:02 +0300

    refactor controller, add logging for db connect

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-require('dotenv').config()
-const http = require('http')
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const config = require('./utils/config')
+const blogsRouter = require("./controllers/blogs")
 const logger = require('./utils/logger')
-const Blog = require("./models/blog")
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
--- models/blog.js
+++ models/blog.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const config = require('../utils/config')
-
+const logger = require('../utils/logger')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -11,7 +11,15 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
 
 module.exports = mongoose.model('Blog', blogSchema)
