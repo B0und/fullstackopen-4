8c3ff26455acffb475790f450a88d9e65d517772
Unbound <f1arenova1@gmail.com>
2021-07-31 20:39:05 +0300

    wip

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,11 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3003;
 const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ps8p8.mongodb.net/phonebook?retryWrites=true&w=majority`;
 
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ?  `mongodb+srv://${process.env.TEST_DB_USER}:${process.env.TEST_DB_PASS}@cluster0.ps8p8.mongodb.net/phonebook?retryWrites=true&w=majority`
+    : `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ps8p8.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
 module.exports = {
   MONGODB_URI,
   PORT,
